6d3d7538cc11a0202ddf94d2ef7c19d1b0cae685
simon salcedo <salcedosimon63@gmail.com>
2026-09-08 17:30:09 -0300

    Agregar endpoint POST move

--- src/server.js
+++ src/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const { moveHandler } = require("./moveHandler");
 
 const app = express();
 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+
+app.post("/move", (req, res) => {
+  const state = req.body;
+
+  const movement = moveHandler(state);
+
+  res.status(200).json(movement);
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
